437602c384fd3e5c00d61c86ddb580798260cc33
Godfred <goddysmart2468@gmail.com>
2024-06-03 18:52:18 -0700

    How many subs task debug

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,10 +9,16 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data.get('data', {}).get('subscribers', 0)
+            return subscribers
+        else:
+            print(f"Error: Received status code {response.status_code}")
+            return 0
+    except requests.RequestException as e:
+        print(f"Request Exception: {e}")
         return 0
+

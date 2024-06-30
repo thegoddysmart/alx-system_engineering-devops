4cc18c44a48201e0905b2b718fa8f7b27e5fa261
Godfred <goddysmart2468@gmail.com>
2024-06-30 00:28:38 -0700

    Debugging task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,9 +12,13 @@ def top_ten(subreddit):
     client_id = "ML7Se32I89MYsuGtpfNQfw"
     client_secret = "SfMV3qA_Tz1PSHiZBhcxge4TfknjPg"
 
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+
     results = requests.get(
         f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10",
-        headers={"User-Agent": "Reddit Top Ten Posts"}, auth=(client_id, client_secret))
+        headers={"User-Agent": "Reddit Top Ten Posts"},
+        auth=(client_id, client_secret))
 
     if results.status_code == 200:
         response_data = results.json()

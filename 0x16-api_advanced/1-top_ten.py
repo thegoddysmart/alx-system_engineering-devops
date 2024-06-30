038c7ff90576c0e8ec53354c68f0b36ed8ab4138
Godfred <goddysmart2468@gmail.com>
2024-06-30 03:54:17 -0700

    debug task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,15 +12,19 @@ def top_ten(subreddit):
     client_id = "ML7Se32I89MYsuGtpfNQfw"
     client_secret = "SfMV3qA_Tz1PSHiZBhcxge4TfknjPg"
 
-    results = requests.get(
-        f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10",
-        headers={"User-Agent": "Reddit Top Ten Posts"},
-        auth=(client_id, client_secret))
+    try:
+        results = requests.get(
+            f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10",
+            headers={"User-Agent": "Reddit Top Ten Posts"},
+            auth=(client_id, client_secret))
 
-    if results.status_code == 200:
-        response_data = results.json()
-        titles = response_data['data']['children']
-        for title in titles:
-            print(title['data']['title'])
-    else:
+        if results.status_code == 200:
+            data = results.json()
+            titles = data['data']['children']
+
+            for title in titles:
+                print(title['data']['title'])
+        else:
+            print(None)
+    except Exception as e:
         print(None)
